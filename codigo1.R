b837fdd032394e8a012610eaef48b0cb8946d744
saritatb2 <Tellezsarita190@gmail.com>
2026-09-11 19:17:20 -0500

    Asignacíón de tipo de variable (discreta o numérica)

--- codigo1.R
+++ codigo1.R
@@ -4,3 +4,15 @@ View(GLMM_Sarita)
 library(lme4)
 library(tidyverse)
 library(performance)
+GLMM_Sarita <- GLMM_Sarita %>%
+  mutate(
+    # Variables continuas (numéricas)
+    ageHabitatyears = as.numeric(ageHabitatyears),
+    areaStand       = as.numeric(areaStand),
+    
+    # Variable discreta / categórica (Factor con sus 3 categorías)
+    understory      = as.factor(understory),
+    
+    # Variable aleatoria (factor)
+    Finca_Lote      = as.factor(Finca_Lote)
+  )
