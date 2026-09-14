b8d39a26be45267f499067af079e8b6348d58730
saritatb2 <Tellezsarita190@gmail.com>
2026-09-14 16:41:56 -0500

    Binomial negativo en algunos casos y final de los GLMM por cada gremio

--- codigo1.R
+++ codigo1.R
@@ -38,6 +38,7 @@ summary(modelo_A_frug)
 check_overdispersion(modelo_A_frug)
 check_zeroinflation(modelo_A_frug)
 
+#Binomial Negativo
 # install.packages("MASS")
 library(MASS)
 modelo_A_frug_nb <- glmer.nb(
@@ -47,3 +48,96 @@ modelo_A_frug_nb <- glmer.nb(
 summary(modelo_A_frug_nb)
 check_overdispersion(modelo_A_frug_nb)
 check_zeroinflation(modelo_A_frug_nb)
+
+#Granivoros
+modelo_R_gran <- glmer(
+  R_Gran ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+summary(modelo_R_gran)
+check_overdispersion(modelo_R_gran)
+check_zeroinflation(modelo_R_gran)
+
+modelo_A_gran <- glmer(
+  A_Gran ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+check_overdispersion(modelo_A_gran)
+summary(modelo_A_gran)
+check_zeroinflation(modelo_A_gran)
+
+#Nectarivoros
+modelo_R_nect <- glmer(
+  R_Nect ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+summary(modelo_R_nect)
+check_overdispersion(modelo_R_nect)
+check_zeroinflation(modelo_R_nect)
+
+modelo_A_nect <- glmer(
+  A_Nect ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+check_overdispersion(modelo_A_nect)
+check_zeroinflation(modelo_A_nect)
+summary(modelo_A_nect)
+
+#Invertivoros R_Binomial Negativa
+modelo_R_inver_nb <- glmer.nb(
+  R_Inver ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data = GLMM_Sarita
+)
+check_overdispersion(modelo_R_inver_nb)
+check_zeroinflation(modelo_R_inver_nb)
+summary(modelo_R_inver_nb)
+
+modelo_A_inver_nb <- glmer.nb(
+  A_Inver ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data = GLMM_Sarita
+)
+
+summary(modelo_A_inver_nb)
+check_overdispersion(modelo_A_inver_nb)
+check_zeroinflation(modelo_A_inver_nb)
+
+#Omnívoros
+modelo_R_omni <- glmer(
+  R_Omni ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+check_overdispersion(modelo_R_omni)
+check_zeroinflation(modelo_R_omni)
+summary(modelo_R_omni)
+
+modelo_A_omni_nb <- glmer.nb(
+  A_Omni ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data = GLMM_Sarita
+)
+check_overdispersion(modelo_A_omni_nb)
+check_zeroinflation(modelo_A_omni_nb)
+summary(modelo_A_omni_nb)
+
+#Vertiboros
+modelo_R_vert <- glmer(
+  R_Vert ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+check_overdispersion(modelo_R_vert)
+check_zeroinflation(modelo_R_vert)
+summary(modelo_R_vert)
+
+modelo_A_vert <- glmer(
+  A_Vert ~ understory + ageHabitatyears + areaStand + (1 | Finca_Lote), 
+  data   = GLMM_Sarita, 
+  family = poisson(link = "log")
+)
+check_overdispersion(modelo_A_vert)
+check_zeroinflation(modelo_A_vert)
+summary(modelo_A_vert)
